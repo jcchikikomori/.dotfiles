ffe4b02f71b3485d7239246e2eeab65f0f6fb6e1
John Cyrill Corsanes <jccorsanes@protonmail.com>
2022-09-18 17:47:16 +0800

    Using distrod

--- wsl-ubuntu/setup.sh
+++ wsl-ubuntu/setup.sh
@@ -4,8 +4,16 @@ sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 sudo apt-get install -y git zsh
 
-git clone https://github.com/DamionGans/ubuntu-wsl2-systemd-script.git /tmp/setup-systemd
-bash /tmp/setup-systemd/ubuntu-wsl2-systemd-script.sh
+# New method on setting up systemctl
+# Source: https://github.com/nullpo-head/wsl-distrod
+curl -L -O "https://raw.githubusercontent.com/nullpo-head/wsl-distrod/main/install.sh"
+chmod +x install.sh
+sudo ./install.sh install
+
+# If you want to automatically start your distro on Windows startup, enable distrod by the following command 
+#/opt/distrod/bin/distrod enable --start-on-windows-boot
+# Otherwise
+/opt/distrod/bin/distrod enable
 
 curl -sS https://starship.rs/install.sh | sh
 curl -L git.io/antigen > $HOME/antigen.zsh
