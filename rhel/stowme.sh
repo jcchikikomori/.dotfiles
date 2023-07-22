c3c61daeef683005a0a4b1a7f4585dcfdb95909c
John Cyrill Corsanes <jccorsanes@protonmail.com>
2023-07-22 11:10:47 +0800

    Testing custom package

--- rhel/stowme.sh
+++ rhel/stowme.sh
@@ -8,4 +8,4 @@ cd $HOME || return
 # Workaround for Fedora
 export LD_PRELOAD="/usr/lib64/libgcrypt.so.20"
 dotstow stow bash antigen tmux tmuxp vim vscode systems dxvk
-dotstow stow zsh git ssh
+dotstow stow zsh git ssh test-fedora
