24ad4133c5699da7d5f9df5e1ae2c8e50d8c683f
John Cyrill Corsanes <jccorsanes@protonmail.com>
2023-09-19 13:24:52 +0800

    Refactor cleanup script part 2

--- cleanup.sh
+++ cleanup.sh
@@ -8,11 +8,16 @@ rm -rf $HOME/.tmux
 rm -f $HOME/.tmux.conf
 rm -f $HOME/.tmux.conf.local
 
+echo 'Cleaning up VSCode...'
+rm -rf $HOME/.vscode
+rm -rf $HOME/.vscode-server
+
 echo 'Cleaning up other files...'
 rm -f $HOME/.vimrc
 rm -f $HOME/.gitconfig
 
 echo 'Unstowing...'
+
 # dotstow unstow bash antigen tmux tmuxp vim vscode systems dxvk
 rm -f $HOME/.alacritty.yml
 rm -f $HOME/.antigenrc
@@ -24,7 +29,7 @@ rm -f $HOME/.hushlogin
 rm -f $HOME/.profile
 rm -f $HOME/.tmuxp
 rm -f $HOME/.vimrc
-rm -f $HOME/.vscode
+
 # dotstow unstow zsh git ssh
 cp -rf $HOME/.zsh $HOME/.backups
 cp -f $HOME/.zshrc $HOME/.backups
@@ -35,6 +40,8 @@ rm -f $HOME/.zsh
 rm -f $HOME/.zshrc
 rm -f $HOME/.zshrc.pre-oh-my-zsh
 rm -f $HOME/.git
+
 # Avoid removing ssh directory
 # rm -f $HOME/.ssh-backup
 # mv $HOME/.ssh $HOME/.ssh-backup
+rm -f $HOME/.ssh/config
