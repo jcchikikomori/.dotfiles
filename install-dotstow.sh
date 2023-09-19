36fe46180b963080cd637aab541cd78bb2fe57eb
John Cyrill Corsanes <jccorsanes@protonmail.com>
2023-09-19 13:37:52 +0800

    Mandate smoke testing on scripts part 1

--- install-dotstow.sh
+++ install-dotstow.sh
@@ -1,5 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
+# Function to check if a command is installed
+check_command() {
+    if ! which "$1" >/dev/null 2>&1; then
+        echo "ERROR: $1 is not installed on this system!"
+        exit 1
+    fi
+}
+
+# Prelim checks
+check_command make
+check_command stow
+check_command git
+
+# Clone dotstow repository and install
 git clone https://github.com/clayrisser/dotstow ~/.dotstow
 cd ~/.dotstow
 sudo make install
