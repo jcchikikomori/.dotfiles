6b6a353b573f64ebfa223c17cae4efff9fe05795
John Cyrill Corsanes <jccorsanes@protonmail.com>
2023-09-27 00:18:02 +0800

    Refactoring for Debian/Ubuntu part 3 - Separate adding user & init

--- debian/init.sh
+++ debian/init.sh
@@ -4,16 +4,3 @@
 apt-get update
 apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 apt-get install -y sudo htop iftop mtr dkms lz4 git zsh stow build-essential sqlite3 ccache tmux
-
-# Create non root user
-useradd johnc -m
-# Using zsh shell
-chsh -s /usr/bin/zsh johnc
-# Add johnc to sudo group
-usermod -aG sudo johnc
-# Setting password
-if [ -z "$SKIP_SETTING_USER" ]; then
-  passwd johnc
-else
-  echo 'Skipped setting password';
-fi
