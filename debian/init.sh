111a13a9fe8804824ce6034a206869cc8f632713
John Cyrill Corsanes <jccorsanes@protonmail.com>
2023-09-27 00:03:52 +0800

    Refactoring for Debian/Ubuntu part 2.1 - Replace the user 'admin' for debian

--- debian/init.sh
+++ debian/init.sh
@@ -6,14 +6,14 @@ apt-get install -y apt-transport-https ca-certificates curl software-properties-
 apt-get install -y sudo htop iftop mtr dkms lz4 git zsh stow build-essential sqlite3 ccache tmux
 
 # Create non root user
-useradd admin -m
+useradd johnc -m
 # Using zsh shell
-chsh -s /usr/bin/zsh admin
-# Add admin to sudo group
-usermod -aG sudo admin
+chsh -s /usr/bin/zsh johnc
+# Add johnc to sudo group
+usermod -aG sudo johnc
 # Setting password
 if [ -z "$SKIP_SETTING_USER" ]; then
-  passwd admin
+  passwd johnc
 else
   echo 'Skipped setting password';
 fi
