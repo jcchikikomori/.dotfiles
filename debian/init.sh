1a095650eec54891b2dd7f4f882ca52a4cf19a44
John Cyrill Corsanes <jccorsanes@protonmail.com>
2023-09-27 00:01:30 +0800

    Refactoring for Debian/Ubuntu part 2 - attempt to fix Ubuntu CI

--- debian/init.sh
+++ debian/init.sh
@@ -2,7 +2,7 @@
 # THIS SCRIPT IS FOR ROOT USER ONLY. DO NOT EXECUTE THE SCRIPT AS NON-ROOT USER!
 
 apt-get update
-apt-get install -y sudo apt-transport-https ca-certificates curl software-properties-common
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 apt-get install -y sudo htop iftop mtr dkms lz4 git zsh stow build-essential sqlite3 ccache tmux
 
 # Create non root user
