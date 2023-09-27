5aa8a57fde06d21b09ede7ff334fcb8a02043f85
John Cyrill Corsanes <jccorsanes@protonmail.com>
2023-09-27 15:55:40 +0800

    Added stow as essentials for Fedora

--- rhel/setup.sh
+++ rhel/setup.sh
@@ -3,7 +3,7 @@
 echo 'Installing dependencies from system...'
 sudo dnf groupinstall -y 'Development Tools'
 sudo dnf install -y gcc-c++ make
-sudo dnf install -y vim nano git zsh unzip
+sudo dnf install -y vim nano htop iftop stow git zsh unzip
 sudo dnf install -y python2 python3 libssh-devel libgcrypt libgcrypt-devel
 sudo dnf install -y python3-tmuxp
 
