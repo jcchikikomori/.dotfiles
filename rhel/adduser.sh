821eacab883202b7dc34c47d79b25d8852a15092
John Cyrill Corsanes <jccorsanes@protonmail.com>
2024-01-24 17:40:02 +0800

    All distros: Changed user to johnc

--- rhel/adduser.sh
+++ rhel/adduser.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # Create non root user
-sudo useradd admin -m
+sudo useradd johnc -m
 # Using zsh shell
-sudo usermod -s /bin/zsh admin
-# Add admin to wheel group
-sudo usermod -aG wheel admin
+sudo usermod -s /bin/zsh johnc
+# Add johnc to wheel group
+sudo usermod -aG wheel johnc
 # Setting password
 if [ -z "$SKIP_SETTING_USER" ]; then
-  sudo passwd admin
+  sudo passwd johnc
 else
   echo 'Skipped setting password';
 fi
