be38b267749aa778f03d64a6649010b6010a4c27
John Cyrill Corsanes <4709030+jcchikikomori@users.noreply.github.com>
2025-02-23 16:23:23 +0800

    Updating setup scripts for Fedora

--- php.sh
+++ php.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+
+# Installing a dependency
+curl -L https://github.com/nih-at/libzip/releases/download/v1.7.3/libzip-1.7.3.tar.gz -o libzip-1.7.3.tar.gz && \
+tar -zxvf libzip-1.7.3.tar.gz && \
+cd libzip-1.7.3 && \
+cmake3 . -DCMAKE_INSTALL_PREFIX=/usr && \
+make && \
+sudo make install
+
 git clone https://github.com/phpenv/phpenv.git ~/.phpenv
 export PATH="$HOME/.phpenv/bin:$PATH"
 eval "$(phpenv init -)"
