a1b550de640a4b6869f2bbf734a08ab42a1b8715
John Cyrill Corsanes <4709030+jcchikikomori@users.noreply.github.com>
2025-02-23 17:30:41 +0800

    [Fedora] Resort with system-wide php instead + Added VS Code

--- php.sh
+++ php.sh
@@ -13,5 +13,5 @@ export PATH="$HOME/.phpenv/bin:$PATH"
 eval "$(phpenv init -)"
 git clone https://github.com/php-build/php-build $(phpenv root)/plugins/php-build
 # Always the updated version of PHP, due to dependency issue on C compiler.
-phpenv install 8.3.13
+# phpenv install 8.3.13
 phpenv rehash
--- rhel/setup.sh
+++ rhel/setup.sh
@@ -6,6 +6,7 @@ sudo dnf install -y gcc-c++ make
 sudo dnf install -y vim nano htop iftop stow git zsh unzip
 sudo dnf install -y python2 python3 libssh-devel libgcrypt libgcrypt-devel
 sudo dnf install -y python3-tmuxp
+sudo dnf install -y php composer
 
 # PHP dependencies
 sudo dnf install -y \
