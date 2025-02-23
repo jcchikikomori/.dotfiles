17e142c771ef5815f59a98ceed336e002fed8aa2
John Cyrill Corsanes <4709030+jcchikikomori@users.noreply.github.com>
2025-02-23 21:40:53 +0800

    Updating setup scripts for Fedora (#46)
    * Updating setup scripts for Fedora
    
    * [Fedora] logic fix on adduser
    
    * [NodeJS] Needed to ensure nvm is loaded upon setup
    
    * Added another vscode directory to ignore
    
    * [Fedora] Resort with system-wide php instead + Added VS Code
    
    * [Fedora] VS Code permission fix

--- php.sh
+++ php.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
+
+# Installing a dependency
+curl -L https://github.com/nih-at/libzip/releases/download/v1.7.3/libzip-1.7.3.tar.gz -o libzip-1.7.3.tar.gz && \
+tar -zxvf libzip-1.7.3.tar.gz && \
+cd libzip-1.7.3 && \
+cmake3 . -DCMAKE_INSTALL_PREFIX=/usr && \
+make && \
+sudo make install
+
 git clone https://github.com/phpenv/phpenv.git ~/.phpenv
 export PATH="$HOME/.phpenv/bin:$PATH"
 eval "$(phpenv init -)"
 git clone https://github.com/php-build/php-build $(phpenv root)/plugins/php-build
 # Always the updated version of PHP, due to dependency issue on C compiler.
-phpenv install 8.3.13
+# phpenv install 8.3.13
 phpenv rehash
--- rhel/adduser.sh
+++ rhel/adduser.sh
@@ -1,14 +1,33 @@
 #!/bin/sh
 
+# Check if a username is provided as an argument
+if [ -n "$1" ]; then
+  USERNAME=$1
+else
+  USERNAME=$(whoami)
+  if [ "$USERNAME" = "root" ]; then
+    USERNAME="johnc"
+  fi
+fi
+
 # Create non root user
-sudo useradd johnc -m
+sudo useradd $USERNAME -m
 # Using zsh shell
 sudo usermod -s /bin/zsh johnc
 # Add johnc to wheel group
 sudo usermod -aG wheel johnc
 # Setting password
+confirm_and_set_password() {
+  read -p "Do you want to set a password for $USERNAME? (y/n): " confirm
+  if [ "$confirm" = "y" ]; then
+    sudo passwd $USERNAME
+  else
+    echo 'Skipped setting password';
+  fi
+}
+
 if [ -z "$SKIP_SETTING_USER" ]; then
-  sudo passwd johnc
+  confirm_and_set_password
 else
   echo 'Skipped setting password';
 fi
