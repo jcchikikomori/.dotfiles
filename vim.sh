5701431a82f3ab62a0e70c93308f35d81f30cead
John Cyrill Corsanes <4709030+jcchikikomori@users.noreply.github.com>
2025-03-01 23:56:11 +0800

    Attempt to optimize vim installation. Maximum timeout - 300 seconds

--- vim.sh
+++ vim.sh
@@ -6,18 +6,27 @@ TIMEOUT=300
 # Create lock file to prevent concurrent installations
 LOCK_FILE="/tmp/vim-plugin-install.lock"
 
+# Store original directory
+ORIGINAL_DIR=$(pwd)
+
+# Create temporary working directory
+WORKDIR=$HOME
+
+# Change to temporary directory
+cd "$WORKDIR"
+
+# Dependency
+echo "Installing vim-plug plugin manager"
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# Ensure .vimrc is loaded
+cp -f linux/vim/.vimrc ~/.vimrc
+
 # Acquire lock
 (
     flock -n 200 || { 
         echo "Error: Another plugin installation is in progress"
         exit 1
     }
-
-    # Dependency
-    echo "Installing vim-plug plugin manager"
-    curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-    # Ensure .vimrc is loaded
-    cp -f linux/vim/.vimrc ~/.vimrc
 
     # Install plugins with verbose logging
     echo "Starting plugin installation..."
@@ -38,3 +47,6 @@ LOCK_FILE="/tmp/vim-plugin-install.lock"
     fi
 
 ) 200>$LOCK_FILE
+
+# Return to original directory
+cd "$ORIGINAL_DIR"
