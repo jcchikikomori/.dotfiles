5db5919d09efd400708a63fc863c9300854a7da2
John Cyrill Corsanes <4709030+jcchikikomori@users.noreply.github.com>
2025-04-18 18:13:43 +0800

    Add user script refactoring

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 mkdir -p $HOME/bin
-rm -rf $HOME/bin/*
+# rm -rf $HOME/bin/*
 cp -rf ./linux/systems/bin/* $HOME/bin/
 
 echo "Welcome! You may proceed to your chosen systems then execute setup.sh"
