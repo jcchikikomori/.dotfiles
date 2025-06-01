ba11a3068e60f1ff2ad2cb5c0a42dedbea4cc962
John Cyrill Corsanes <4709030+jcchikikomori@users.noreply.github.com>
2025-06-01 22:02:59 +0800

    Added lindbergh package

--- arch/stowme.sh
+++ arch/stowme.sh
@@ -7,5 +7,5 @@
 sh $HOME/.dotfiles/linux/zsh/bin/dotfiles-cleanup
 sh $HOME/.dotfiles/linux/zsh/bin/dotfiles-ssh
 cd $HOME || return
-dotstow stow bash zsh git antigen tmux tmuxp vim vscode dxvk systems flatpak alacritty wireplumber
+dotstow stow bash zsh git antigen tmux tmuxp vim vscode dxvk systems flatpak alacritty wireplumber lindbergh
 exit 0
