8156516d53bd43dc62c44213cff299a29f3a2f83
John Cyrill Corsanes <4709030+jcchikikomori@users.noreply.github.com>
2025-09-24 17:55:54 +0800

    [refactor] add lindbergh package to everyone

--- ubuntu/stowme.sh
+++ ubuntu/stowme.sh
@@ -2,5 +2,5 @@
 sh $HOME/.dotfiles/linux/systems/.local/bin/org.jcchikikomori.dotfiles/bin/dotfiles-cleanup
 sh $HOME/.dotfiles/linux/systems/.local/bin/org.jcchikikomori.dotfiles/bin/dotfiles-ssh
 cd $HOME || return
-dotstow stow bash zsh git antigen tmux tmuxp vim vscode dxvk systems flatpak alacritty wireplumber flags
+dotstow stow bash zsh git antigen tmux tmuxp vim vscode dxvk systems flatpak alacritty wireplumber flags lindbergh
 exit 0
