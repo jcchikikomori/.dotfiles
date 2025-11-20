8ce99fb6ae8834d22c178db3be315ab5f2478757
John Cyrill Corsanes <4709030+jcchikikomori@users.noreply.github.com>
2025-11-20 19:12:07 +0800

    refactor(termux): setup fix

--- termux/setup.sh
+++ termux/setup.sh
@@ -5,16 +5,19 @@
 # Note: vim-gtk3 (gvim) is not available in Termux
 # Note: Additional build dependencies may not be available in Termux
 # Change mirrors first based on your location
+# Note: Execute `export TERM=xterm-256color` if you face any terminal issues
+export TERM=xterm-256color
 termux-change-repo
 pkg update
 
-pkg install -y apt-transport-https ca-certificates curl software-properties-common
-pkg install -y stow vim nano htop iftop mtr git zsh build-essential
-pkg install -y python zip openssh xsel ncdu wget tmux unzip
+pkg install -y apt ca-certificates curl
+pkg install -y stow vim nano htop git zsh build-essential
+pkg install -y python zip openssh ncdu wget tmux unzip
 
 # Setting default locale
 # Termux does not use loadkeys or localectl, locale settings are managed differently
-termux-reload-settings
+# Workaround: termux-reload-settings may fail with app_process errors in some environments
+termux-reload-settings 2>/dev/null || echo "Warning: termux-reload-settings not available, skipping..."
 sed -i '/^# *en_US.UTF-8 UTF-8/s/^# *//' $PREFIX/etc/locale.gen
 locale-gen en_US.UTF-8
 export LANG=en_US.UTF-8
