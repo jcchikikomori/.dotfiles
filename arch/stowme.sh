5625afa7fca558242dc78364a945be16dd534129
John Cyrill Corsanes <4709030+jcchikikomori@users.noreply.github.com>
2025-11-29 23:32:36 +0800

    refactor(sh): tentative fix for #90

--- arch/stowme.sh
+++ arch/stowme.sh
@@ -8,4 +8,5 @@ sh $HOME/.dotfiles/linux/systems/.local/bin/org.jcchikikomori.dotfiles/bin/dotfi
 sh $HOME/.dotfiles/linux/systems/.local/bin/org.jcchikikomori.dotfiles/bin/dotfiles-ssh
 cd $HOME || return
 dotstow stow bash zsh git antigen tmux tmuxp vim vscode dxvk systems flatpak alacritty wireplumber flags lindbergh starship
+git reset --hard HEAD
 exit 0
